865e59478c44171a4af5b64d2685f09362aef3a6
Jhu, Sin-Han <blueice0916@gmail.com>
2021-09-20 04:27:51 +0800

    feat(pages/_app.js, pages/index.js): 在getStaticProps建立基本的graphQL資料獲取

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Layout from '../components/layout/Layout'
 import '../styles/globals.scss'
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Layout>
   )
 }
 
+export { client }
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import { GET_LAUNCHES_LIST } from '../graphQL'
+import { client } from './_app'
 import Styled from '../styles/Home.module.scss'
 
-export default function Home() {
+export default function Home(props) {
+  console.log('🚀 ~ file: index.js ~ line 6 ~ Home ~ props', props)
   return <div className={Styled.container}>Home page</div>
+}
+export const getStaticProps = async () => {
+  const { data } = await client.query({ query: GET_LAUNCHES_LIST })
+  return {
+    props: {
+      launches: data.launchesPast,
+    },
+  }
 }
