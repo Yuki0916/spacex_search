bbc72f971690a632e561cf894d7c2f5fb9959d3a
Jhu, Sin-Han <blueice0916@gmail.com>
2021-09-22 06:26:59 +0800

    feat(common/LaunchCard): 實作動態跳轉至詳細任務頁面

--- components/common/LaunchCard.js
+++ components/common/LaunchCard.js
@@ -1,31 +1,38 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-// ! 僅完成切版，尚未完成跳轉功能 先暫時忽略eslint error
 import Image from 'next/image'
+import Link from 'next/link'
 import Styled from './LaunchCard.module.scss'
 
 function LaunchCard({ id, imageUrl, launchDate, missionName, details }) {
   return (
     <div className={Styled.Container}>
       {imageUrl && (
-        <div className={Styled.ImageContainer}>
-          <Image
-            className={Styled.Image}
-            src={imageUrl}
-            alt={`LaunchImage${id}`}
-            layout='fill'
-            objectFit='cover'
-          ></Image>
-        </div>
+        <Link href={`launch/${id}`} passHref>
+          <div className={Styled.ImageContainer}>
+            <Image
+              className={Styled.Image}
+              src={imageUrl}
+              alt={`LaunchImage${id}`}
+              layout='fill'
+              objectFit='cover'
+            ></Image>
+          </div>
+        </Link>
       )}
-      <div className={Styled.Title}>
-        <a href='#'>{missionName}</a>
-      </div>
-      <div className={Styled.Time}>
-        <a href='#'>{launchDate}</a>
-      </div>
-      <div className={Styled.Content}>
-        <a href='#'>{details}</a>
-      </div>
+      <Link href={`launch/${id}`} passHref>
+        <div className={Styled.Title}>
+          <a>{missionName}</a>
+        </div>
+      </Link>
+      <Link href={`launch/${id}`} passHref>
+        <div className={Styled.Time}>
+          <a>{launchDate}</a>
+        </div>
+      </Link>
+      <Link href={`launch/${id}`} passHref>
+        <div className={Styled.Content}>
+          <a>{details}</a>
+        </div>
+      </Link>
     </div>
   )
 }
