61bf9d1f6a5543ad1258bb755a58369bed231abe
Jhu, Sin-Han <blueice0916@gmail.com>
2021-09-23 23:55:45 +0800

    feat(component/layout): 添加header, 讓詳細資料頁可以回到主頁

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Styled from './Layout.module.scss'
 
 export default function Layout(porps) {
   return (
-    <div>
+    <div className={Styled.container}>
       <div className={Styled.backgroundImage}>
         <Image
           src='https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
@@ -11,6 +12,11 @@ export default function Layout(porps) {
           layout='fill'
           objectFit='cover'
         />
+      </div>
+      <div className={Styled.header}>
+        <Link href='/' passHref>
+          SpaceX LaunchList
+        </Link>
       </div>
       {porps.children}
     </div>
